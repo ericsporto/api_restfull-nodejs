1efb3efeaf2f4e2d24cd7e91bd7375c0cb91ab7a
Eric de Souza Porto Carvalho <ericsporto@gmail.com>
2023-08-06 16:16:05 -0300

    feat: post route testing by insomnia

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = 3000;
+
+//indica para o express ler o body com json
+app.use(express.json())
+
+//mock
+const teams =[
+    {id: 1 , team: 'Brasil', group: 'G'},
+    {id: 2 , team: 'Argentina', group: 'A'},
+    {id: 3 , team: 'França', group: 'D'},
+    {id: 4 , team: 'Portugal', group: 'H'},
+]
 
 // rota padrão ou raiz
 app.get('/', (req, res) => {
   res.send('Deu certo, ok!');
 });
 
-// escutar a porta 3000
-app.listen(port, () => {
-  console.log(`Servidor rodando no endereço http://localhost:${port}`);
+app.get('/teams', (req, res) => {
+  res.status(200).send(teams);
 });
+app.post('/teams', (req, res) => {
+  teams.push(req.body)
+  res.status(201).send('Team add successful')
+});
+
+export default app;
