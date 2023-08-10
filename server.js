7c7c34b39940190d1118a335ab1abfb8050a3991
Eric de Souza Porto Carvalho <ericsporto@gmail.com>
2023-08-10 19:18:22 -0300

    refactor: to MVC

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import app from './src/app.js'
+import app from './src/app.js';
+
 const PORT = 3000;
 
-// escutar a porta 3000
-app.listen(PORT, () => {
-  console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
-});
+   // escutar a porta 3000
+   app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+  });
--- src/app.js
+++ src/app.js
@@ -1,29 +1,86 @@
 import express from 'express';
+import connection from '../infra/connection.js';
 
 const app = express();
 
 //indica para o express ler o body com json
-app.use(express.json())
+app.use(express.json());
 
-//mock
-const teams =[
-    {id: 1 , team: 'Brasil', group: 'G'},
-    {id: 2 , team: 'Argentina', group: 'A'},
-    {id: 3 , team: 'França', group: 'D'},
-    {id: 4 , team: 'Portugal', group: 'H'},
-]
+//função que filtra por Id
+const searchTeamById = (id) => {
+  return teams.find((team) => team.id == id);
+};
 
-// rota padrão ou raiz
-app.get('/', (req, res) => {
-  res.send('Deu certo, ok!');
-});
+//função que deleta por Id
+const deleteTeamById = (id) => {
+  return teams.findIndex((team) => team.id == id);
+};
 
+//ROTAS
+//lista os times
 app.get('/teams', (req, res) => {
-  res.status(200).send(teams);
+  const sql = 'SELECT * FROM teams;';
+  connection.query(sql, (error, result) => {
+    if (error) {
+      res.status(404).json({ error: error });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
+
+//filtra o time por id
+app.get('/teams/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'SELECT * FROM teams WHERE id=?;';
+  connection.query(sql, id, (error, result) => {
+    const line = result[0];
+    if (error) {
+      res.status(404).json({ error: error });
+    } else {
+      res.status(200).json(line);
+    }
+  });
+});
+
+//adiciona um time na lista
 app.post('/teams', (req, res) => {
-  teams.push(req.body)
-  res.status(201).send('Team add successful')
+  const team = req.body;
+  const sql = 'INSERT INTO teams SET ?;';
+  connection.query(sql, team, (error, result) => {
+    if (error) {
+      res.status(400).json({ error: error });
+    } else {
+      res.status(201).json(result);
+    }
+  });
+});
+
+//deleta um time da lista por id
+app.delete('/teams/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'DELETE FROM teams WHERE id=?;';
+  connection.query(sql, id, (error, result) => {
+    if (error) {
+      res.status(404).json({ error: error });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+//edita um time por id
+app.put('/teams/:id', (req, res) => {
+  const id = req.params.id;
+  const team = req.body;
+  const sql = 'UPDATE teams SET ? WHERE id=?;';
+  connection.query(sql, [team, id], (error, result) => {
+    if (error) {
+      res.status(400).json({ error: error });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 export default app;
