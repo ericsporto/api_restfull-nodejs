679cacbc6ce0d55c86cd1133109a34c12fb4286f
Eric de Souza Porto Carvalho <ericsporto@gmail.com>
2023-08-11 07:19:24 -0300

    feat: insert routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 import express from 'express';
-import TeamController from './app/controllers/TeamController.js';
+import routes from './routes.js';
 
 const app = express();
+
+//usar o routes
+app.use(routes)
 
 //indica para o express ler o body com json
 app.use(express.json());
 
-//ROTAS
-app.get('/teams', TeamController.index);
-app.get('/teams/:id', TeamController.show);
-app.post('/teams', TeamController.store);
-app.put('/teams/:id', TeamController.update);
-app.delete('/teams/:id', TeamController.delete);
 
 export default app;
